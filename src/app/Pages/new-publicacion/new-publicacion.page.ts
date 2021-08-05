48039a8e14e189f2f843988e20a4497213c77939
wendysotoA <wendy.soto@itdeabs.com>
2021-08-05 12:08:00 -0500

    add Publication--- desing Form

--- src/app/Pages/new-publicacion/new-publicacion.page.ts
+++ src/app/Pages/new-publicacion/new-publicacion.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, fromCollectionRef } from '@angular/fire/firestore';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FireauthService } from 'src/app/services/fireauth.service';
+import { MateriasService } from 'src/app/services/materias.service';
+import { MateriasInterface } from 'src/app/shared/materias-interface';
+import { PublicacionInterface } from 'src/app/shared/publicacion';
 
 @Component({
   selector: 'app-new-publicacion',
@@ -6,10 +12,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-publicacion.page.scss'],
 })
 export class NewPublicacionPage implements OnInit {
+  userInfo: any;
+  idUser: string;
+  listaMaterias: MateriasInterface[];
+  carreraId: string;
 
-  constructor() { }
+  public newPublicacion=new FormGroup({
+    materia:new FormControl(),
+    categoria:new FormControl(),
+    titulo:new FormControl(),
+    descripcion:new FormControl(),
+    file:new FormControl()
+  });
+
+  constructor(
+    private materiasServ: MateriasService,
+    private firestore: AngularFirestore,
+    private serviceauth: FireauthService
+  ) {
+     //INFORMACION DE USUARIO ACTUAL
+     this.serviceauth.stateAuth().subscribe(user => {
+      if (user != null) {
+        this.idUser = user.uid;
+        console.log(this.idUser);
+        this.getuser(this.idUser);
+      }
+    });
+  }
+
+
+
 
   ngOnInit() {
   }
+  ///OBTENER INFO  USUARIO DE LA BDD
+  public getuser(uid: string) {
+    const docRef = this.firestore.collection('Usuarios').doc(uid);
+    docRef.get().toPromise().then((doc) => {
+      if (doc.exists) {
+        //console.log('infoUser', doc.data());
+        this.userInfo = doc.data();
+        this.carreraId = this.userInfo.carrera;
+        console.log(this.carreraId);
+        //LISTAR MATERIAS DEL USUARIO
+        this.obtenerMaterias(this.carreraId);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log('"no existe el usuario"');
+      }
+    }).catch((error) => {
+      console.log('erro', error);
+    });
 
+  }
+
+
+  //OBTENER MATERIAS DEL Usuario
+  obtenerMaterias(idC: string) {
+
+    const path = 'Materias';
+    this.materiasServ.getCollection<MateriasInterface>(path).subscribe(res => {
+
+      this.listaMaterias = res.filter(e => idC === e.idCarrera);
+
+      console.log(this.listaMaterias);
+
+      this.listaMaterias.forEach(element => {
+        this.carreraId = element.id;
+        console.log(element.id);
+      });
+
+
+    });
+  }
+
+  //GUARDAR NUEVA PUBLICACION
+  savePublicacion(publi: PublicacionInterface){
+try {
+console.log(this.newPublicacion.value);
+
+} catch (error) {
+console.log(error);
+
+}
+
+  }
 }
--- src/app/shared/publicacion.ts
+++ src/app/shared/publicacion.ts
@@ -1,7 +1,7 @@
 export interface PublicacionInterface {
   id: string;
   idUser: string;
-  idTarea: string;
+  idMateria: string;
   categoria: string;
   fecha: Date;
   likes: number;
