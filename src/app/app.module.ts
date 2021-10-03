8c6e091ebc7154a688284f1fadd4aa33bad3ce2c
wendysoto <wendy.soto@epn.edu.ec>
2021-10-03 10:05:07 -0500

    despliegue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,30 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+//firebaseAuth
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { PreviewAnyFile } from '@ionic-native/preview-any-file/ngx';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+   //importar librerias
+   AngularFireAuthModule,
+   AngularFireDatabaseModule,
+   AngularFireModule.initializeApp(environment.firebaseConfig),
+   AngularFireStorageModule,
+   FormsModule,
+   ReactiveFormsModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
